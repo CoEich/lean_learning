fbd9a0a39b7fa897fc095247a85188e2fc2f7de7
constantin <constantin.eichenberg@mailbox.org>
2026-01-07 03:20:55 +0100

    more comments and 'have'/'show'

--- LeanLearning/theorem_proving_in_lean/chapter_3_exercises.lean
+++ LeanLearning/theorem_proving_in_lean/chapter_3_exercises.lean
@@ -1,17 +1,29 @@
 /- De Morgan rule -/
 
 def de_morgan {p q: Prop} : ¬ (p ∨ q) ↔ ¬ p ∧ ¬ q :=
+  -- we need to provide two proofs as arguments to 'Iff.intro'
   Iff.intro
     (
       λ (f: ¬ (p ∨ q)) =>
-        have hnp: ¬ p := λ (hp : p) => f (Or.inl hp)
-        have hnq: ¬ q := λ (hq : q) => f (Or.inr hq)
+        -- we are gonna construct terms for not p, not q respecively and apply the and constructor
+
+        -- use 'have' for local definitions, here we get witnesses of not p and not q from f
+        have hnp: ¬ p := λ (hp : p) =>
+          have hpl: p ∨ q := Or.inl hp
+          -- use 'show' for syntactic sugar, the expression '⟨hnp, hnq⟩' would suffice here
+          show False from f hpl
+        -- same reasoning for q
+        have hnq: ¬ q := λ (hq : q) =>
+          have hqr: p ∨ q := Or.inr hq
+          show False from f hqr
+
         show ¬ p ∧ ¬ q from ⟨hnp, hnq⟩
     )
     (
       λ (h : ¬ p ∧ ¬ q) =>
         have hnp: ¬ p := h.left
         have hnq: ¬ q := h.right
+        -- we show the conclusion here by applying the case-analysis, i.e the eliminator for coproducts
         show ¬ (p ∨ q) from λ (hpq : p ∨ q) =>
           Or.elim hpq hnp hnq
     )
