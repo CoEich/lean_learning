8fe579f169911d75d039124d56e0574100c4a804
constantin <constantin.eichenberg@mailbox.org>
2026-01-09 02:27:55 +0100

    clean up main and lib file

--- LeanLearning.lean
+++ LeanLearning.lean
@@ -1,2 +1,5 @@
 -- This module serves as the root of the `LeanLearning` library.
 -- Import modules here that should be built as part of the library.
+
+import LeanLearning.theorem_proving_in_lean.chapter_3_exercises
+import LeanLearning.homotopy_type_theory_book.chapter_one_exercises
--- Main.lean
+++ Main.lean
@@ -1,4 +1,1 @@
 import LeanLearning
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
